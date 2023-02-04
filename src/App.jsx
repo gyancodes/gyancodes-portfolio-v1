0c98aaf8bc7ed9ae7da1dc0dc0e912981f3dd276
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-02-05 01:48:11 +0530

    ui update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/layouts/Header";
+import Hero from "./components/pages/Hero";
+import Skills from "./components/pages/Skills";
 
 function App() {
 
   return (
     <>
      <Header/>
+     <Hero/>
+     <Skills/>
     </>
   );
 }
---
+++ src/components/pages/Skills.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import HTML from "./../../assets/html5.svg";
+import CSS from "./../../assets/css3.svg";
+import JS from "./../../assets/javascript.svg";
+import ReactJS from "./../../assets/react.svg";
+import NodeJS from "./../../assets/nodedotjs.svg";
+
+const Skills = () => {
+  return (
+    <>
+      <div className="mx-auto  max-w-3xl items-center justify-between mt-14 p-4 font-mono">
+        <span className="text-2xl font-semibold mb-9 border-b-4 border-black">Skills</span>
+        <div className="flex gap-4 mt-9">
+          <img src={HTML} className="h-12 " alt="" />
+          <img src={CSS} className="h-12" alt="" />
+          <img src={JS} className="h-12" alt="" />
+          <img src={ReactJS} className="h-12" alt="" />
+        </div>
+        <div className="flex gap-4 mt-6">
+          <img src={NodeJS} className="h-12" alt="" />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg"
+            className="h-12"
+          />
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+            className="h-12"
+          />
+
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" className="h-12" />
+
+        </div>
+        <div className="flex gap-4 mt-6">
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" className="h-12" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original-wordmark.svg" className="h-12" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" className="h-12" />
+
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" className="h-12" />
+
+
+
+
+
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
